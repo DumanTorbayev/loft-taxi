e52199c5fe7fd81903705f1d22c9267c75a8a20c
Duman Torbayev <dumanchikt@gmail.com>
2021-09-24 12:46:24 +0300

    add gp-pages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {Provider} from "react-redux";
 import {store} from "./store";
 
 ReactDOM.render(
-    <Provider store={store}>
+    <Provider store={store} basename={process.env.PUBLIC_URL}>
         <BrowserRouter>
             <App/>
         </BrowserRouter>
